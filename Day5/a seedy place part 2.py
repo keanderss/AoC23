0a1be6863fe843df35a0912384bacf09fdd9b50d
keanderss <kenzu.2000172@gmail.com>
2023-12-07 08:14:11 +0200

    Day 5 part 2 episode 3 chapter 2 page 7

--- Day5/a seedy place part 2.py
+++ Day5/a seedy place part 2.py
@@ -16,41 +16,34 @@ locations = []
 def convert(m, i):
     # print()
     # print("Starting conversion!")
-    for j in range(len(m)):
-        mapj0 = m[j][0]
-        mapj0int = int(mapj0)
-        mapj1 = m[j][1]
-        mapj1int = int(mapj1)
-        mapj2 = m[j][2]
-        mapj2int = int(mapj2)
+    for j in range(3):
+        #mapj0 = m[j][0]
+        #mapj0int = int(m[j][0])
+        #mapj1 = m[j][1]
+        mapj1int = int(m[j][1])
+        #mapj2 = m[j][2]
+        #mapj2int = int(m[j][2])
         # print("Searching for " + str(i) + " in " + str(range(mapj1int, mapj1int + mapj2int)))
-        diff = mapj0int - mapj1int
+        #diff = mapj0int - mapj1int
         # print("j: " + str(j))
         # print("i: " + str(i))
         # print(mapj0 + " " + mapj1 + " " + mapj2)
         # print("diff: " + str(diff))
-        idiff = i + diff
+        # idiff = i + (mapj0int - mapj1int)
         # print("i + diff: " + str(idiff))
-        if i in range(mapj1int, mapj1int + mapj2int):
+        if i in range(mapj1int, mapj1int + int(m[j][2])):
             # print("Match for " + str(i) + " found in " + str(range(mapj1int, mapj1int + mapj2int)))
-            return idiff
+            return i + (int(m[j][0]) - mapj1int)
     # print("Returning default value: " + str(i))
     # print()
     return i
 
 
 def getseedlocation(seed):
-    soil = convert(seedtosoil, seed)
-    fertilizer = convert(soiltofertilizer, soil)
-    water = convert(fertilizertowater, fertilizer)
-    light = convert(watertolight, water)
-    temperature = convert(lighttotemperature, light)
-    humidity = convert(temperaturetohumidity, temperature)
-    location = convert(humiditytolocation, humidity)
-    locations.append(location)
-    print("Seed " + str(seed) + ", soil " + str(soil) + ", fertilizer " + str(fertilizer) + ", water " + str(
-        water) + ", light " + str(light) + ", temperature " + str(temperature) + ", humidity " + str(
-        humidity) + ", location " + str(location) + ".\n")
+    locations.append(convert(humiditytolocation, convert(temperaturetohumidity, convert(lighttotemperature, convert(watertolight, convert(fertilizertowater, convert(soiltofertilizer, convert(seedtosoil, seed))))))))
+    #print("Seed " + str(seed) + ", soil " + str(soil) + ", fertilizer " + str(fertilizer) + ", water " + str(
+    #    water) + ", light " + str(light) + ", temperature " + str(temperature) + ", humidity " + str(
+    #    humidity) + ", location " + str(location) + ".\n")
 
 
 def createmap(map, i):
@@ -67,7 +60,7 @@ def createmap(map, i):
 def printmap(m):
     i = 0
     for y in m:
-        print("y" + str(i) + ": \t" + str(y) + "\n")
+        print("y" + str(i) + ": \t" + str(y))
         i += 1
 
 
@@ -79,44 +72,44 @@ def initialize():
         i += 1
         line = linecache.getline(filename, i)
         if line.startswith("seeds:"):
-            print("Loading seeds!\n")
+            print("Loading seeds!")
             linearrway = line.split()
             for j in linearrway:
                 if j.isnumeric():
                     seedranges.append(j)
             print(seedranges)
         elif line.startswith("seed-to-soil map:"):
-            print("Creating seed to soil map!\n")
+            print("Creating seed to soil map!")
             createmap(seedtosoil, i + 1)
             print(seedtosoil)
             mapcount += 1
         elif line.startswith("soil-to-fertilizer map:"):
-            print("Creating soil to fertilizer map!\n")
+            print("Creating soil to fertilizer map!")
             createmap(soiltofertilizer, i + 1)
             print(soiltofertilizer)
             mapcount += 1
         elif line.startswith("fertilizer-to-water map:"):
-            print("Creating fertilizer to water map!\n")
+            print("Creating fertilizer to water map!")
             createmap(fertilizertowater, i + 1)
             print(fertilizertowater)
             mapcount += 1
         elif line.startswith("water-to-light map:"):
-            print("Creating water to light map!\n")
+            print("Creating water to light map!")
             createmap(watertolight, i + 1)
             print(watertolight)
             mapcount += 1
         elif line.startswith("light-to-temperature map:"):
-            print("Creating light to temperature map!\n")
+            print("Creating light to temperature map!")
             createmap(lighttotemperature, i + 1)
             print(lighttotemperature)
             mapcount += 1
         elif line.startswith("temperature-to-humidity map:"):
-            print("Creating temperature to humidity map!\n")
+            print("Creating temperature to humidity map!")
             createmap(temperaturetohumidity, i + 1)
             print(temperaturetohumidity)
             mapcount += 1
         elif line.startswith("humidity-to-location map:"):
-            print("Creating humidity to location map!\n")
+            print("Creating humidity to location map!")
             createmap(humiditytolocation, i + 1)
             print(humiditytolocation)
             mapcount += 1
@@ -124,7 +117,7 @@ def initialize():
             if mapcount == 7:
                 break
             if i > 999:
-                print("Initialization error!\n")
+                print("Initialization error!")
                 break
 
 
@@ -148,24 +141,24 @@ rangepairs = []
 rangestarts = []
 rangeends = []
 def run():
-    print(seedranges)
+    # print(seedranges)
     for i in range(len(seedranges)):
         if i % 2 == 0:
             seedi = seedranges[i]
-            print("seedi: " + seedi)
+            #print("seedi: " + seedi)
             seedinti = int(seedi)
             seedp1 = seedranges[i + 1]
-            print("seedp1: " + seedp1)
+            #print("seedp1: " + seedp1)
             seedintp1 = int(seedp1)
             pair = (seedinti, seedinti + seedintp1)
             rangestarts.append(seedinti)
             rangeends.append(seedinti + seedintp1)
             rangepairs.append(pair)
-            print(rangepairs)
+            #print(rangepairs)
 
 def scanrange(start, stop):
     for s in range(start, stop):
-        print("range: " + str(start) + " " + str(stop))
+        #print("range: " + str(start) + " " + str(stop))
         getseedlocation(s)
 run()
 
@@ -191,7 +184,7 @@ for t in threads:
 trunning = len(threads)
 while trunning > 0:
     trunning = len(threads)
-    print("Active threads: " + str(trunning) + "\n")
+    #print("Active threads: " + str(trunning) + "\n")
     for t in threads:
         if not t.is_alive():
             t.join()
@@ -199,4 +192,4 @@ while trunning > 0:
 
 print("locations: " + str(locations))
 if len(locations) > 0:
-    print("Lowest location: " + str(min(locations)) + "\n")
+    print("Lowest location: " + str(min(locations)))
